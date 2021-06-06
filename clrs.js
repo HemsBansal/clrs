bcc98df7c5127d2723cacbf6c086d6f22f2c5412
Hemant Kumar <hemsbansal@gmail.com>
2021-06-06 13:46:09 +0530

    Add files via upload

---
+++ clrs.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+    document.querySelector('nav').classList.toggle('active-nav');
+    document.querySelector('.menu').classList.toggle('active-menu');
+});
